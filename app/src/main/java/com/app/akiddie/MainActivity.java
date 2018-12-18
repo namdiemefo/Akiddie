af9d7d684ccc16e3f37b31406b2909da7865d04a
Namdi <nnamdi_ned@yahoo.com>
2018-12-18 09:28:55 +0100

    general progress and implemented the side drawer in main activity

--- app/src/main/java/com/app/akiddie/MainActivity.java
+++ app/src/main/java/com/app/akiddie/MainActivity.java
@@ -1,11 +1,18 @@
 package com.app.akiddie;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.app.akiddie.Adapters.BookAdapter;
 import com.app.akiddie.Entities.BooksObject;
@@ -17,12 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView bookRecyclerView;
+    private Toolbar toolbar;
+    private DrawerLayout mDrawableLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         bookRecyclerView = findViewById(R.id.akiddie_recycler);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, 2);
@@ -32,7 +44,11 @@ public class MainActivity extends AppCompatActivity {
         BookAdapter bookAdapter = new BookAdapter(MainActivity.this, getAllBooks());
         bookRecyclerView.setAdapter(bookAdapter);
 
+        mDrawableLayout = findViewById(R.id.drawer_layout);
+
+
     }
+
 
     private List<BooksObject> getAllBooks() {
         List<BooksObject> books = new ArrayList<BooksObject>();
@@ -44,5 +60,24 @@ public class MainActivity extends AppCompatActivity {
         books.add((new BooksObject(5, " Chisom", R.drawable.artboard_5)));
         books.add(new BooksObject(6, "Kelvin", R.drawable.artboard_6));
         return books;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Context context;
+        switch (item.getItemId()) {
+            case R.id.book_icon:
+                mDrawableLayout.openDrawer(GravityCompat.START);
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
